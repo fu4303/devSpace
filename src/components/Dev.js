ef85c08ef0143c334a45509cbc4c61921935c20e
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-03-03 20:31:49 -0500

    Update Dev.js

--- src/components/Dev.js
+++ src/components/Dev.js
@@ -41,7 +41,7 @@ const Dev = () => {
                         < ThreeDots fill="#6366F1" />
                     </div>
                     :
-                    <div className="flex flex-row flex-wrap justify-center p-2">
+                    <div className="flex flex-row flex-wrap justify-center space-y-4 p-2">
 
                         {devPosts.map(post => {
                             return (
@@ -49,7 +49,7 @@ const Dev = () => {
                                     <a href={post.url}
                                         target="_blank" rel="noreferrer"
                                     >
-                                        <div className="w-90 h-72 py-auto shadow flex flex-col bg-white rounded-md p-4 text-left m-2">
+                                        <div className="w-80 h-72 py-auto shadow flex flex-col bg-white rounded-md p-4 text-left m-2">
                                             <img src={post.social_image === '' ? 'https://picsum.photos/seed/picsum/200/150' : post.social_image}
                                                 alt="cover-img" className="rounded" />
                                             <h3 className="text-xl mt-2">
